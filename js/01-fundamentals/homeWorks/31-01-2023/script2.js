183bedc434421c78ab21e0ea4a74f89afd124a64
Mihail Prokopchik <raedar5555@gmail.com>
2023-02-01 16:58:25 +0300

    Исправил ошибки в script2,4 и добавил информацию о типе symbol

--- js/01-fundamentals/homeWorks/31-01-2023/script2.js
+++ js/01-fundamentals/homeWorks/31-01-2023/script2.js
@@ -3,6 +3,5 @@
 alert('Программа вычисляющая Ваш год рождения');
 
 let years = Number(prompt('Сколько Вам лет'));
-let yourYearOfBirth = 'Ваш год рождения';
 
-alert(2022 - years + yourYearOfBirth);
+alert(2022 - years + ' Ваш год рождения');
