cb6db0f3f03103cb0e790aaab4364908b693c004
Mihail Prokopchik <raedar5555@gmail.com>
2023-02-01 12:50:22 +0300

    Создал программу вычисляющую объём комнаты

---
+++ js/01-fundamentals/homeWorks/31-01-2023/script7.js
@@ -0,0 +1,9 @@
+'use strict';
+
+alert('Программа вычисляющая объём комнаты');
+
+let long = Number(prompt('Длинна комнаты'));
+let width = Number(prompt('Ширина комнаты'));
+let height = Number(prompt('Высота комнаты'));
+
+alert('Объём комнаты ' + long * width * height);
