dc0b9a7196eabea4350927bdfe1c6c3adb678879
Mihail Prokopchik <raedar5555@gmail.com>
2023-02-01 11:07:13 +0300

    Создал программу вычисляющую год рождения по количеству лет

---
+++ js/01-fundamentals/homeWorks/31-01-2023/script2.js
@@ -0,0 +1,8 @@
+'use strict';
+
+alert('Программа вычисляющая Ваш год рождения');
+
+let years = Number(prompt('Сколько Вам лет'));
+let yourYearOfBirth = 'Ваш год рождения';
+
+Number(alert(2022 - years));
