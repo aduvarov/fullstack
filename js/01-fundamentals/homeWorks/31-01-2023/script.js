4861446710a4bf898bf095ec4dda05cc6dc7d7c1
Mihail Prokopchik <raedar5555@gmail.com>
2023-02-01 10:44:33 +0300

    Создал программу выводящую пирамиду в консоль

---
+++ js/01-fundamentals/homeWorks/31-01-2023/script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('Программа выводящая в консоль пирамиду');
+
+let firstStage = '#';
+let secondStage = '##';
+let thirdStage = '###';
+let fourthStage = '####';
+let fifthStage = '#####';
+
+console.log(firstStage);
+console.log(secondStage);
+console.log(thirdStage);
+console.log(fourthStage);
+console.log(fifthStage);
