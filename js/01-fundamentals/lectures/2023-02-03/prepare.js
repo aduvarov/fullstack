a265c3d89439d0ca33c3543dece9736b149b277d
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2023-02-03 20:02:51 +0600

    update: prepare files

---
+++ js/01-fundamentals/lectures/2023-02-03/prepare.js
@@ -0,0 +1,15 @@
+'use strict';
+
+console.log('js' + 90, 'You answer: ');
+console.log('js' + true, 'You answer: ');
+console.log('js' + undefined, 'You answer: ');
+console.log('js' + null, 'You answer: ');
+console.log('' + 15, 'You answer: ');
+console.log('5' + 10, 'You answer: ');
+console.log(+'5' + 15, 'You answer: ');
+console.log(+true, 'You answer: ');
+console.log(+'true', 'You answer: ');
+console.log(+undefined, 'You answer: ');
+console.log(+'5' * 10, 'You answer: ');
+console.log('5' * 10, 'You answer: ');
+console.log(5 * +'10' + Number(undefined), 'You answer: ');
