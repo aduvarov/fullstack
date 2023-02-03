d91c0cdb5253f5da58702e10a16eef41ddd96d40
Mihail Prokopchik <raedar5555@gmail.com>
2023-02-03 11:43:19 +0300

    Изучил преобразование типов,операторы

---
+++ js/01-fundamentals/lectures/2023-02-01/script1.js
@@ -0,0 +1,61 @@
+'use strict';
+
+// let string2 = '5';
+// let result = 3 + string2 + 'NaN';
+
+// console.log(result);
+
+// String
+// Number  (NaN, Infinity, -Infinity)
+// BitInt
+// Boolean (true, false)
+// Undefined
+// null
+// Symbol
+// Object
+
+// let amountBanknot = null;
+// let banknotaType = 10;
+
+// let summaDeneg = amountBanknot * banknotaType;
+
+// console.log(summaDeneg);
+
+// let a = 3;
+// let b = 3;
+// let result = a * b;
+// console.log(result);
+
+// const PI = 3.14;
+
+// const myLastName = 'Prokopchik';
+
+// let radius = 10;
+// let result = 4 * PI * radius ** 2;
+
+// console.log(result);
+
+// console.log(17 % 5); //2
+// console.log(120 % 10); //0
+// console.log(3 % 2); //1
+// console.log(24 % 5); //4
+
+// console.log(10 + 20 + 5 + 100 + '0')
+
+// let result = 2 + 1 * 3 + (2 + 1)
+
+// let x = 10;
+// let y = 20;
+
+// let z = x * y + (x = 1 * 1); // x1
+
+// console.log(z); // 21
+
+// let startPointA = startPointB = startPointC = 10;
+
+let apples = 5;
+
+console.log(apples++);
+
+apples += 2;
+console.log(--apples);
