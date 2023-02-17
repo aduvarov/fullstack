48f88b2592a469f06d861f908acee4af5f52995a
unknown <raedar5555@gmail.com>
2023-02-17 16:14:28 +0300

    Лекция

--- js/01-fundamentals/lectures/2023-02-15/prepare.js
+++ js/01-fundamentals/lectures/2023-02-15/prepare.js
@@ -1,21 +1,21 @@
 'use strict';
 
-console.log('true || NaN', 'Your answer: ');
-console.log('60+15' - '60-15' - 60 / 15, 'Your answer: ');
-console.log(true || NaN, 'Your answer: ');
-console.log(Infinity, 'Your answer: ');
-console.log(-Infinity, 'Your answer: ');
-console.log(checkUserPassword('misha', '12345678'), 'Your answer: ');
+console.log('true || NaN', 'Your answer: true || NaN');
+console.log('60+15' - '60-15' - 60 / 15, 'Your answer: NaN ');
+console.log(true || NaN, 'Your answer: true');
+console.log(Infinity, 'Your answer: Infinity ');
+console.log(-Infinity, 'Your answer:-Infinity');
+console.log(checkUserPassword('misha', '12345678'), 'Your answer: true');
 
 let greetUser = function (userName) {
     console.log('Привет ' + userName);
 };
 greetUser('Михаил');
-console.log('Your answer: ');
+console.log('Your answer: Привет Михаил');
 
 function checkUserPassword(user, password) {
     const correctUser = 'misha';
-    const correctPassword = 12345678;
+    const correctPassword = '12345678';
     if (correctUser !== user) {
         return false;
     }
