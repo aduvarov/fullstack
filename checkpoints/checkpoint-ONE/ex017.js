b1d1b3c538ef80157b9c16b20c3144ff697c84ef
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-22 20:54:54 +0300

    add: ex017 (средний возраст)

---
+++ checkpoints/checkpoint-ONE/ex017.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//Возраст Тани – X лет, а возраст Мити – Y лет. Найти их средний возраст, а также определить, на сколько отличается возраст каждого ребенка от среднего значения.
+// Резуьтаты можно вывести через на консоль.
+
+let yearsTanya = 16;
+let yearsMitya = 14;
+let averageAge = (yearsMitya + yearsTanya) / 2;
+console.log(averageAge, yearsMitya - averageAge, yearsTanya - averageAge);
