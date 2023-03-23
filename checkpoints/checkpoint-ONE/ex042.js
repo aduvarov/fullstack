fc367135964544157b6f43c5de935ffe5c35e22c
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-23 22:40:44 +0300

    add: ex043 (Определить: поместится ли круг в квадрат и наоборот.)

--- checkpoints/checkpoint-ONE/ex042.js
+++ checkpoints/checkpoint-ONE/ex042.js
@@ -8,6 +8,6 @@ let squareSide = 16;
 function areaCircleAndSquare(squareSide, circleRadius) {
     let pi = Math.PI.toFixed(2);
     let sCircle = pi * circleRadius ** 2;
-    let diagonalSquare = Math.sqrt(squareSide) * 2;
+    let sSquare = squareSide ** 2;
     return { circle: sCircle.toFixed(1), square: sSquare.toFixed(1) };
 }
