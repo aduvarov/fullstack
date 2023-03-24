422aec96a44629e7edc49974a93c7ca638bf56ea
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-24 17:26:20 +0300

    ex046 (Определить: поместится ли треугольник в круг и наоборот..

---
+++ checkpoints/checkpoint-ONE/ex046.js
@@ -0,0 +1,20 @@
+'use strict';
+
+//ex046:
+// Известны площади круга и равностороннего треугольни-
+// ка. Определить:
+// а) уместится ли круг в треугольнике?
+// б) уместится ли треугольник в круге?
+let circleS = 78.5;
+let triangleS = 16;
+
+function areaCircleAndSquare(triangleS, circleS) {
+    let pi = Math.PI.toFixed(2);
+    let radiusCircle = Math.trunc(Math.sqrt(circleS / pi));
+    let sideTriangle = Math.trunc(Math.sqrt((4 * triangleS) / Math.sqrt(3)));
+    if (sideTriangle <= radiusCircle * Math.sqrt(3)) {
+        console.log('triangle in circle: true');
+    } else {
+        console.log('circle in triangle: true');
+    }
+}
