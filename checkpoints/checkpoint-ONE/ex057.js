1e75dae980a86c5b2d15e9494725fcb31cb14922
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-24 20:03:18 +0300

    add: ex057 (выполняется ли неравенство)

---
+++ checkpoints/checkpoint-ONE/ex057.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Даны три вещественных числа a, b, c. Проверить:
+// а) выполняется ли неравенство a < b < c;
+// б) выполняется ли неравенство b > a > c.
+// Результат в виде объекта
+// { 'a < b < c': true, 'b > a > c': false }
+
+let a = 6;
+let b = 7;
+let c = 8;
+
+function inequality(a, b, c) {
+    if (a < b < c) {
+        return { 'a < b < c': true, 'b > a > c': false };
+    } else if (a > b > c) {
+        return { 'a < b < c': false, 'b > a > c': true };
+    } else {
+        return { 'a < b < c': false, 'b > a > c': false };
+    }
+}
