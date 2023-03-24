ca03bcdfe93de05f0057d83d72d3d2a591694c5e
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-24 20:23:34 +0300

    add: ex059 (Определить, войдет ли в конверт с внутренними размерами)

---
+++ checkpoints/checkpoint-ONE/ex059.js
@@ -0,0 +1,22 @@
+'use strcit';
+
+//ex059:
+// Определить, войдет ли в конверт с внутренними размерами a и b мм прямоугольная открытка с размерами с и d мм. Для размещения открытки в конверте необходим зазор в 1 мм с каждой стороны. Необходимо проверить оба варианта размещения открытки.
+// Результат boolean
+
+let envelopeLength = 50;
+let envelopeWidth = 40;
+let postcardLength = 48;
+let postcardWidth = 38;
+
+function postcardInEnvelope(envelopeLength, envelopeWidth, postcardLength, postcardWidth) {
+    if (envelopeLength - postcardLength >= 2) {
+        if (envelopeWidth - postcardWidth >= 2) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+}
