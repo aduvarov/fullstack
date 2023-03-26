be9d2296fbbe21a67b202502963a1d67aabd8e47
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-26 22:00:15 +0300

    add: ex089(Напечатать таблицу умножения на 9)

---
+++ checkpoints/checkpoint-ONE/ex089.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//Напечатать таблицу умножения на 9:
+// 9 х 1 = 9
+// 9 х 2 = 18
+// ...
+// 9 х 9 = 81
+
+let num = 0;
+let count = 0;
+
+document.write('<table border="1">');
+for (let row = 0; row < 10; row++) {
+    for (let col = 0; col < 1; col++) {
+        num = num + 9;
+        count = count + 1;
+        document.write(`<td> ${count} x 9 = ${num}</td>`);
+    }
+    document.write('</tr>');
+}
+document.write('</table>');
