7c9814885616aa2ae6eacea17131110e30cca3fb
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-27 19:58:55 +0300

    add: ex097(какой урожай будет собран за первые шесть лет в общем.)

---
+++ checkpoints/checkpoint-ONE/ex097.js
@@ -0,0 +1,19 @@
+'use strict';
+
+//В некотором году (назовем его условно первым) на участке
+//  в 100 га средняя урожайность ячменя составила 20 ц с гектара.
+// После этого каждый год площадь участка увеличивалась на 5 %,
+// а средняя урожайность – на 2 %. Определить: какой урожай будет собран за первые шесть лет в общем.
+// result number
+
+let site = 100;
+let barleyYield = 20;
+
+let summa = 0;
+
+for (let count = 1; count <= 7; count++) {
+    site = site * 0.05 + site;
+    barleyYield = barleyYield * 0.02 + barleyYield;
+    summa = summa + site * barleyYield;
+}
+console.log(summa.toFixed(1));
