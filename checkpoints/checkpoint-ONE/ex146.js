962d1c2a6668840e6f3ce676c94566645d8c76e9
Mihail Prokopchik <raedar5555@gmail.com>
2023-03-30 20:11:54 +0300

    ex146(Написать функцию исключающую из этого массива повторяющиеся элементы.)

---
+++ checkpoints/checkpoint-ONE/ex146.js
@@ -0,0 +1,32 @@
+'use strict';
+
+//ex146:
+// Дан массив целых чисел. Используя функцию из задания ex136 сгенирировать массив из 100 элементов, с интервалом от 1 до 100
+// включительно. Написать функцию исключающую из этого массива повторяющиеся элементы. Т.е. чтобы в этом массиве были только уникальные числа. Новый массив может быть меньше по размеру.
+
+let a = 1;
+let b = 100;
+let n = 100;
+
+function interval(a, b, n) {
+    if (n > a + b) {
+        return [];
+    }
+    let numbers = [];
+    let rnd;
+    for (let index = 0; index < n; index++) {
+        rnd = randomInteger(a, b);
+        numbers.push(rnd);
+    }
+    let result = [];
+    for (let str of numbers) {
+        if (!result.includes(str)) {
+            result.push(str);
+        }
+    }
+    return result;
+}
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
