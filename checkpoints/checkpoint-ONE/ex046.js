334617630c026f5e022bd0e3c139ad727b3a4484
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-02 18:26:53 +0300

    fix: ex046(Исправил ошибку с ответом)

--- checkpoints/checkpoint-ONE/ex046.js
+++ checkpoints/checkpoint-ONE/ex046.js
@@ -12,9 +12,11 @@ function areaCircleAndSquare(triangleS, circleS) {
     let pi = Math.PI.toFixed(2);
     let radiusCircle = Math.trunc(Math.sqrt(circleS / pi));
     let sideTriangle = Math.trunc(Math.sqrt((4 * triangleS) / Math.sqrt(3)));
-    if (sideTriangle <= radiusCircle * Math.sqrt(3)) {
+    if (sideTriangle < radiusCircle * Math.sqrt(3)) {
         console.log('triangle in circle: true');
-    } else {
+    } else if (sideTriangle > radiusCircle * Math.sqrt(3)) {
         console.log('circle in triangle: true');
+    } else {
+        console.log('false');
     }
 }
