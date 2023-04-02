f85eed0003253faef2ed92e4ceccbf0a92a41ce8
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-02 18:23:59 +0300

    fix: ex045(Исправил ошибку с датой)

--- checkpoints/checkpoint-ONE/ex045.js
+++ checkpoints/checkpoint-ONE/ex045.js
@@ -3,20 +3,34 @@
 //ex045:
 // Известны год и номер месяца рождения человека, а также год и номер месяца сегодняшнего дня (январь – 1 и т. д.). Определить возраст человека (число полных лет). В случае совпадения указанных номеров месяцев считать, что прошел полный год.
 
-let dateOfBirth = new Date('2005-08-10');
+let dateOfBirth = new Date('2023-04-03');
 
 function humanAge(dateOfBirth) {
     let yearBirth = dateOfBirth.getFullYear();
     let monthBirth = dateOfBirth.getMonth() + 1;
+    let dayOfBirth = dateOfBirth.getDate();
     let now = new Date();
     let monthNow = now.getMonth() + 1;
     let yearNow = now.getFullYear();
+    let dayNow = now.getDate();
     let years = 0;
+    if (monthBirth === monthNow) {
+        if (dayOfBirth < dayNow) {
+            years = yearNow - yearBirth;
+            console.log(years);
+        } else if (dayOfBirth > dayNow) {
+            years = yearNow - yearBirth;
+            console.log(years);
+        } else {
+            years = yearNow - yearBirth;
+            console.log(years);
+        }
+    }
     if (monthBirth < monthNow) {
         years = yearNow - yearBirth;
         console.log(years);
     }
-    if (monthBirth >= monthNow) {
+    if (monthBirth > monthNow) {
         years = yearNow - yearBirth - 1;
         console.log(years);
     }
