6a0e4a7657a8502f57a1a36b4ffd7d0b06621c2e
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-02 19:07:14 +0300

    fix: ex064(Добавил проверку)

--- checkpoints/checkpoint-ONE/ex064.js
+++ checkpoints/checkpoint-ONE/ex064.js
@@ -6,7 +6,9 @@
 let num = 2400;
 
 function leapYear(num) {
-    if (num % 4 === 0) {
+    if (num % 100 === 0 && num % 400 === 0) {
+        return true;
+    } else if (num % 4 === 0 && num % 100 !== 0) {
         return true;
     } else {
         return false;
