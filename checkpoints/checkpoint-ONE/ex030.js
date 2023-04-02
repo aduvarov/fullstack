945ce6a60c0e6235f2f2af46d62d8f98026da699
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-02 17:59:02 +0300

    fix: ex030(сделал ввод числа с клавиатуры)

--- checkpoints/checkpoint-ONE/ex030.js
+++ checkpoints/checkpoint-ONE/ex030.js
@@ -2,7 +2,7 @@
 
 // <!-- Написать программу, в которой рассчитывается сумма цифр двузначного числа, вводимого с клавиатуры. -->
 
-let num = 24;
+let num = +prompt('Введите двузначное число');
 
 function number(num) {
     let sum = Number(num.toString()[0]) + Number(num.toString()[1]);
