15bf69a235ae10eeb477bab96073d1fd9b96f151
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-03 17:35:32 +0300

    fix:ex120 (Переделал задание)

--- checkpoints/checkpoint-ONE/ex120.js
+++ checkpoints/checkpoint-ONE/ex120.js
@@ -6,19 +6,45 @@
 // ции гонки в сек.). Выводить на экран (console.log()) лучший результат после ввода
 // результата очередного спортсмена. Заканчивать программу по нажатии кнопки отмена.
 
-function bestResult() {
+function bestResultAtlete() {
     let results = [];
     let resultAthlete = 0;
-
-    for (let index = 0; index < 5; index++) {
-        resultAthlete = +prompt('Введите затраченное время');
-        results.push(resultAthlete);
-    }
     let best = 0;
-    for (let index = 0; index < results.length; index++) {
-        if (results[index] > results[index + 1]) {
-            best = results[index + 1];
+    let contin = true;
+    while (contin) {
+        resultAthlete = prompt('Введите затраченное время');
+        if (resultAthlete === null) {
+            contin = false;
         }
+        resultAthlete = +resultAthlete;
+        if (isNaN(resultAthlete)) {
+            console.log('Ошибка: Введены не числовые данные');
+            continue;
+        }
+        results.push(resultAthlete);
+        if (results.length === 1) {
+            best = results[0];
+        } // 22, 15, 13
+        for (let index = 1; index < results.length; index++) {
+            if (best > results[index]) {
+                best = results[index];
+            }
+        }
+        console.log(best);
     }
-    return best;
+
+    // for (let index = 0; index < 5; index++) {
+    // resultAthlete = +prompt('Введите затраченное время');
+    // if (resultAthlete === 0) {
+    //     return null;
+    // }
+    //     results.push(resultAthlete);
+    //     for (let index = 0; index < results.length; index++) {
+    //         if (results[index] < results[index + 1]) {
+    //             best = results[index + 1];
+    //         }
+    //     }
+    // console.log(best);
+    // }
+    //
 }
