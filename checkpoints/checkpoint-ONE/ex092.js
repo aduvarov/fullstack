0ca00a4938357daa2d4414f604216a9adde14a07
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-03 13:50:42 +0300

    fix:ex092 (Исправил значение суммы)

--- checkpoints/checkpoint-ONE/ex092.js
+++ checkpoints/checkpoint-ONE/ex092.js
@@ -3,9 +3,9 @@
 //Найти среднее арифметическое всех целых чисел от 1 до 750;
 
 let a = 1;
-let b = 750;
+let b = 3;
 let medium = 0;
-let summa = 1;
+let summa = 0;
 let num = 0;
 
 for (let count = a; count <= b; count++) {
