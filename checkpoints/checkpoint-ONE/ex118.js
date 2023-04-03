f9197abf9567bcdce5e6a694b744f82134e7f8f0
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-03 16:49:29 +0300

    fix:ex118 (Сделал решение через while)

--- checkpoints/checkpoint-ONE/ex118.js
+++ checkpoints/checkpoint-ONE/ex118.js
@@ -5,13 +5,12 @@
 let n = 205;
 
 function nDegreeThree(n) {
-    for (let index = 0; index < n; index++) {
-        if (n !== 1 && n !== 3) {
-            n = n / 3;
-        }
-        if (n / 3 === 1 || n === 3) {
+    while (n >= 1) {
+        n = n / 3;
+        if (n / 3 === 1) {
             return true;
+        } else if (n % 3 !== 0) {
+            return false;
         }
     }
-    return false;
 }
