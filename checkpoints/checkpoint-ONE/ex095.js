a160dd6e52ae796ed33c766116e7d6b9df8da9a7
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-03 14:01:49 +0300

    fix:ex095 (Исправил значение суммы)

--- checkpoints/checkpoint-ONE/ex095.js
+++ checkpoints/checkpoint-ONE/ex095.js
@@ -5,12 +5,11 @@
 //ющейся суммы. Определить: сумму вклада через три, шесть и двенадцать месяцев.
 //Ответ в видео объекта
 
-let summa = 6000;
+let summa = 1000;
 let profit = [];
-for (let count = 3; count <= 12; count++) {
-    count = count + 2;
+for (let count = 1; count <= 12; count++) {
     summa = summa * 0.02 + summa;
     profit.push(summa);
 }
 
-console.log({ summa1: profit[0], summa2: profit[1], summa3: profit[3] });
+console.log({ summa1: profit[2].toFixed(1), summa2: profit[5].toFixed(1), summa3: profit[11].toFixed(1) });
