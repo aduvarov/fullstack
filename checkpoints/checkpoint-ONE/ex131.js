4b591f7a34f066bab603b60253ecd69d8490ed29
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-05 11:56:54 +0300

    fix: ex131(Изменил счёт участников игры)

--- checkpoints/checkpoint-ONE/ex131.js
+++ checkpoints/checkpoint-ONE/ex131.js
@@ -43,40 +43,6 @@ function dropoutRate(players, dice) {
     return { winner: results[winner], players: playersResults };
 }
 
-// function dropoutRate(player1, player2, dice) {
-//     let rnd = 0;
-//     let rnd1 = 0;
-//     for (let index = 0; index < dice; index++) {
-//         rnd = randomInteger(1, 6);
-//         rnd1 = randomInteger(1, 6);
-//         player1 += rnd + rnd1;
-//         if (rnd === rnd1) {
-//             rnd = randomInteger(1, 6);
-//             rnd1 = randomInteger(1, 6);
-//             player1 += rnd + rnd1;
-//         }
-//     }
-//     for (let index = 0; index < dice; index++) {
-//         rnd = randomInteger(1, 6);
-//         rnd1 = randomInteger(1, 6);
-//         player2 += rnd + rnd1;
-//         if (rnd === rnd1) {
-//             rnd = randomInteger(1, 6);
-//             rnd1 = randomInteger(1, 6);
-//             player2 += rnd + rnd1;
-//         }
-//     }
-//     if (player1 > player2) {
-//         console.log('player1 win');
-//     }
-//     if (player1 < player2) {
-//         console.log('player2 win');
-//     } else {
-//         console.log('ничья');
-//     }
-//     return { player1: player1, player2: player2 };
-// }
-
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
