73c9d132b1ccae0137d19d4d7458d02e17db29cd
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-05 11:56:03 +0300

    fix: ex121(Изменил счёт последоательности)

--- checkpoints/checkpoint-ONE/ex121.js
+++ checkpoints/checkpoint-ONE/ex121.js
@@ -9,8 +9,12 @@ function longestStretch(m) {
     let count = 0;
     let counter = [];
     for (let index = 0; index < m.toString().length; index++) {
-        if ((Number(m.toString()[index]) === 0 && Number(m.toString()[index + 1])) === 0) {
+        if (+m.toString()[index] === 0 && +m.toString()[index + 1] === 0) {
             count++;
+        } else if (+m.toString()[index] === 0) {
+            counter.push(count + 1);
+        } else if (count === 0) {
+            counter.push(count);
         } else {
             counter.push(count + 1);
             count = 0;
@@ -18,9 +22,9 @@ function longestStretch(m) {
     }
     let best = 0;
     for (let index = 0; index < counter.length; index++) {
-        if (counter[index] < counter[index + 1]) {
-            best = counter[index + 1];
+        if (best < counter[index]) {
+            best = counter[index];
         }
     }
-    return best;
+    return `${best} ${counter}`;
 }
