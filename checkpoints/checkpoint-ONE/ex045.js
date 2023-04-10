c55db16d980ed06f538143280d2a3277fecaaf06
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-10 17:07:39 +0300

    fix: ex045(В случае совпадения указанных номеров месяцев считать, что прошел полный год.)

--- checkpoints/checkpoint-ONE/ex045.js
+++ checkpoints/checkpoint-ONE/ex045.js
@@ -3,7 +3,7 @@
 //ex045:
 // Известны год и номер месяца рождения человека, а также год и номер месяца сегодняшнего дня (январь – 1 и т. д.). Определить возраст человека (число полных лет). В случае совпадения указанных номеров месяцев считать, что прошел полный год.
 
-let dateOfBirth = new Date('2023-04-03');
+let dateOfBirth = new Date('2005-03-10');
 
 function humanAge(dateOfBirth) {
     let yearBirth = dateOfBirth.getFullYear();
@@ -15,16 +15,8 @@ function humanAge(dateOfBirth) {
     let dayNow = now.getDate();
     let years = 0;
     if (monthBirth === monthNow) {
-        if (dayOfBirth < dayNow) {
-            years = yearNow - yearBirth;
-            console.log(years);
-        } else if (dayOfBirth > dayNow) {
-            years = yearNow - yearBirth;
-            console.log(years);
-        } else {
-            years = yearNow - yearBirth;
-            console.log(years);
-        }
+        years = yearNow - yearBirth;
+        console.log(years);
     }
     if (monthBirth < monthNow) {
         years = yearNow - yearBirth;
