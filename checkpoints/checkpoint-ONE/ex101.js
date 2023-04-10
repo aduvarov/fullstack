a03cee4f7a06dfbe33c11faf03356d5f15bbdb47
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-10 19:07:46 +0300

    fix: ex101(Использовал includes)

--- checkpoints/checkpoint-ONE/ex101.js
+++ checkpoints/checkpoint-ONE/ex101.js
@@ -8,7 +8,7 @@ let n = 5;
 let num = [];
 
 for (let index = 10; index < 99; index++) {
-    if (Number(index.toString()[1]) === 5 || Number(index.toString()[0]) === 5) {
+    if (index.toString().includes(5)) {
         num.push(index);
     } else if (index % 5 === 0) {
         num.push(index);
