7504c3cc32b6630af7f4f1ac8ac032971fadf0b2
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-10 19:00:40 +0300

    fix: ex087 (ответ ввиде см)

--- checkpoints/checkpoint-ONE/ex087.js
+++ checkpoints/checkpoint-ONE/ex087.js
@@ -4,8 +4,8 @@
 // тиметры для значений 10, 11, ..., 22 дюйма (1 дюйм = 25,4 мм).
 
 let inch = 9;
-let centimeter = 228.6;
-
+let mm = 228.6;
+let centimeter = 0;
 document.write('<table border="1">');
 for (let row = 0; row < 13; row++) {
     // document.write('<tr>');
@@ -15,8 +15,9 @@ for (let row = 0; row < 13; row++) {
         document.write(`<td> ${inch} фунт </td>`);
     }
     for (let col1 = 0; col1 < 1; col1++) {
-        centimeter = centimeter + 25.4;
-        document.write(`<td> ${centimeter.toFixed(1)}г </td>`);
+        mm = mm + 25.4;
+        centimeter = mm / 10;
+        document.write(`<td> ${centimeter.toFixed(1)}см </td>`);
     }
     document.write('</tr>');
 }
