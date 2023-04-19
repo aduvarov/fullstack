1bea436bf1de21364f2fa6fa18f2fb88c545e639
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-19 18:02:05 +0300

    fix: ex150(определить на каком этаже в каких номерах квартир живут одиночки)

--- checkpoints/checkpoint-ONE/ex150.js
+++ checkpoints/checkpoint-ONE/ex150.js
@@ -2,7 +2,7 @@
 
 //Смоделировать 5 этажный дом, с 4мя подъездами, и 3 квартирами на каждом этаже. С помощью функции ex136 заселить
 // в каждую квартиру от 1 до 5 человек.
-// Вернуть объект содержащий сколько всего жителей живёт в этом доме (number), определить на каком этаже всего дома живёт меньше всего народу (array этажей таких может быть несколько), определить в каких номерах квартир живут одиночки (array)
+// Вернуть объект содержащий сколько всего жителей живёт в этом доме (number), определить на каком этаже всего дома живёт меньше всего народу (array этажей таких может быть несколько), определить в каких номерах квартир живут одиночки (array) {allResidens: number, minResidentsOnFloor: array, singles: array}
 
 let a = 1;
 let b = 5;
@@ -10,49 +10,57 @@ function searchFlat(a, b) {
     let residents = [];
     let rnd;
     let summaFloor = [];
-    let summa = 0;
-    for (let entrance = 1; entrance <= 4; entrance++) {
-        for (let floor = 1; floor <= 5; floor++) {
+    let allResidents = 0;
+    let result = {
+        allResidents: 0,
+        minResidentsOnFloor: [],
+        singles: [],
+    };
+    let flatNum = 1;
+    //заселяем жильцов
+    for (let entrance = 0; entrance < 4; entrance++) {
+        residents[entrance] = [];
+        for (let floor = 0; floor < 5; floor++) {
+            residents[entrance][floor] = [];
             for (let allFlat = 1; allFlat <= 3; allFlat++) {
                 rnd = randomInteger(a, b);
-                summa = summa + rnd;
-                residents.push(rnd);
+                allResidents = allResidents + rnd;
+                residents[entrance][floor].push(rnd);
             }
-            summaFloor.push(summa);
-            summa = 0;
         }
     }
-    summaFloor.sort(compareNumbers);
-    residents.sort(compareNumbers);
-    let smallFloor = summaFloor[0];
-    for (let index = 0; index < summaFloor.length; index++) {
-        if (summaFloor[index] > summaFloor[index + 1]) {
-            smallFloor = summaFloor[index + 1];
+    result.allResidents = allResidents;
+
+    //Ищем на каком этаже во всём доме живёт меньше всего людей
+    residents.forEach(entrance => {
+        entrance.forEach((floor, index) => {
+            if (summaFloor[index] === undefined) {
+                summaFloor[index] = 0;
+            }
+            summaFloor[index] += floor.reduce((acc, residents) => (acc += residents), 0);
+        });
+    });
+    let min = Math.min(...summaFloor);
+    for (let i = 0; i < 5; i++) {
+        if (summaFloor[i] === min) {
+            result.minResidentsOnFloor.push(i + 1);
         }
     }
-    let smallFloorLive = [];
-    for (let index = 1; index < summaFloor.length; index++) {
-        if (summaFloor[index] === smallFloor) {
-            smallFloorLive.push(index);
+
+    //    номера квартир где живут одиночки
+    for (let entrance = 0; entrance < 4; entrance++) {
+        for (let floor = 0; floor < 5; floor++) {
+            for (let flat = 0; flat < 3; flat++) {
+                if (residents[entrance][floor][flat] === 1) {
+                    result.singles.push(flatNum);
+                }
+                flatNum++;
+            }
         }
     }
-    smallFloorLive.push(smallFloor);
-    let allResidents = 0;
-    for (let index = 0; index < residents.length; index++) {
-        allResidents = allResidents + residents[index];
-    }
-    let singles = [];
-    for (let index = 0; index < residents.length; index++) {
-        if (residents[index] === 1) {
-            singles.push(index);
-        }
-    }
-    return `Всех жителей:${allResidents} Квартиры одиночкек ${singles} этажи где живет меньше всего людей ${smallFloorLive} `;
+    return result;
 }
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
-}
-function compareNumbers(a, b) {
-    return a - b;
 }
