87fc9a871dd36c5070aaaf3f9fb514db5f87dd45
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-21 19:40:04 +0300

    fix: ex144(Найти преобладающее направление ветра в своём городе и его среднюю интенсивность)

--- checkpoints/checkpoint-ONE/ex144.js
+++ checkpoints/checkpoint-ONE/ex144.js
@@ -13,77 +13,76 @@
 // }
 // Данные в массивы необходимо собрать с сайта вручную. Необходимо проходить в объекте все виды ветров, даже если там нет данных. Это надо делать с помощью цикла for in. А у массивов необходимо использовать метод .forEach().
 
-let obj = {
+let city = {
     country: 'Беларусь',
-    city: 'Ганцевичи',
+    cityName: 'Ганцевичи',
+    month: 'Jan',
+    winds: {
+        N: [3, 1], //2
+        NE: [2, 3, 4, 3, 1, 3], //6
+        E: [2, 2, 3], //3
+        SE: [2, 4, 3, 3], //4
+        S: [3, 4, 5, 3, 3], //5
+        SW: [5, 3, 3, 4, 4, 6], //6
+        W: [7, 4, 1, 2, 4], //5
+        NW: [], //0
+    },
+    decades: {
+        1: [5, 3, 7, 3, 4, 1, 2, 4, 2, 2],
+        2: [3, 3, 4, 4, 5, 3, 3, 3, 2, 3],
+        3: [5, 4, 3, 1, 2, 3, 3, 1, 4, 6, 4],
+    },
 };
 
-function prevalentWindDirection(obj) {
-    let winds = {
-        N: [3, 1],
-        NE: [2, 3, 4, 3, 1, 3],
-        E: [2, 2, 3],
-        SE: [2, 4, 3, 3],
-        S: [3, 4, 5, 3, 3],
-        SW: [5, 3, 3, 4, 4, 6],
-        W: [7, 4, 1, 2, 4],
-        NW: [],
+let result = prevalentWindDirection(city);
+
+function prevalentWindDirection(city) {
+    let result = {
+        country: city.country,
+        city: city.cityName,
+        prevalentwWindDirection: '',
+        averageWindSpeed: 0,
+        minimumWindDecade: 0,
+        month: city.month,
     };
-    let nodWind = [];
-    let windsL = [];
-    let summa;
-    for (let key in winds) {
-        summa = 0;
-        winds[key].forEach(element => {
-            summa = summa + element;
-        });
-        nodWind.push(summa);
-        nodWind.push(winds[key].length);
-        winds[key] = summa;
-    }
-    let medSpeed = 0;
-    let medSpeedWind = 0;
-    for (let index = 0; index < nodWind.length; index++) {
-        if (medSpeed < nodWind[index]) {
-            medSpeed = nodWind[index];
-            medSpeedWind = nodWind[index + 1];
+    let winds = Object.entries(city.winds);
+    let maxWind = winds[0][1].length;
+
+    for (let wind of winds) {
+        if (wind[1].length > maxWind) {
+            maxWind = wind[1].length;
         }
-    }
-    for (let key in winds) {
-        if (winds[key] === medSpeed) {
-            obj.prevalentwWindDirection = key;
-            obj.averageWindSpeed = (medSpeed / medSpeedWind).toFixed(1);
-        }
-    }
-    let month = {
-        decade1: [5, 3, 7, 3, 4, 1, 2, 4, 2, 2],
-        decade2: [3, 3, 4, 4, 5, 3, 3, 3, 2, 3],
-        decade3: [5, 4, 3, 1, 2, 3, 3, 1, 4, 6, 4],
-    };
-    let sum = [];
-    let summa1;
-    let nod1;
-    for (let key in month) {
-        summa1 = 0;
-        month[key].forEach(element => {
-            summa1 = summa1 + element;
-            return key;
-        });
-        nod1 = Math.trunc(summa1 / month[key].length);
-        month[key] = nod1;
-        sum.push(nod1);
     }
 
-    let minDecade = sum[0];
-    for (let index = 0; index < sum.length; index++) {
-        if (sum[index] > sum[index + 1]) {
-            minDecade = sum[index + 1];
+    let pretendents = winds.filter(wind => wind[1].length === maxWind);
+    let windMax = pretendents[0][1].reduce((acc, wind) => acc + wind, 0);
+    let winner = pretendents[0];
+
+    for (let wind of pretendents) {
+        let windSum = wind[1].reduce((acc, wind) => acc + wind, 0);
+        if (windSum > windMax) {
+            windMax = windSum;
+            winner = wind;
         }
     }
-    for (let key in month) {
-        if (month[key] === minDecade) {
-            obj.minimumWindDecade = key;
-            return obj;
-        }
+    result.prevalentwWindDirection = winner[0];
+    let sum = winner[1].reduce((acc, element) => {
+        acc += element;
+        return acc;
+    }, 0);
+    result.averageWindSpeed = (sum / winner[1].length).toFixed(1);
+
+    let decade1 = city.decades[1].reduce((acc, element) => (acc += element)) / city.decades[1].length; //3
+    let decade2 = city.decades[2].reduce((acc, element) => (acc += element)) / city.decades[2].length; //2
+    let decade3 = city.decades[3].reduce((acc, element) => (acc += element)) / city.decades[3].length; //2
+    if (decade1 <= decade2 && decade1 <= decade3) {
+        result.minimumWindDecade = 1;
+    } else if (decade2 <= decade1 && decade2 <= decade3) {
+        result.minimumWindDecade = 2;
+    } else {
+        result.minimumWindDecade = 3;
     }
+    return result;
 }
+
+console.log(result);
