5fc257646d9405f670e78b37cdba5da196093230
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-23 17:11:39 +0300

    fix: ex073(Выразить стоимость в рублях и копейках)

--- checkpoints/checkpoint-ONE/ex073.js
+++ checkpoints/checkpoint-ONE/ex073.js
@@ -10,45 +10,41 @@ let n = 1230;
 function moneyTransfer(n) {
     let rub = 0;
     let kopeyki = 0;
+    let rubName;
+    let kopName;
     if (1 <= n && n <= 9999) {
         rub = Math.trunc(n / 100);
         kopeyki = n % 100;
         if (rub % 10 == 1 && rub % 100 !== 11 && rub != 11) {
-            if (kopeyki % 10 == 1 && kopeyki % 100 !== 11 && kopeyki != 11) {
-                return `${rub} рубль ${kopeyki} копейка`;
-            } else if (kopeyki % 100 >= 12 && kopeyki % 100 <= 14) {
-                return `${rub} рубль ${kopeyki} копеек`;
-            } else if (kopeyki % 10 == 2 || kopeyki % 10 == 3 || kopeyki % 10 == 4) {
-                return `${rub} рубль ${kopeyki} копейки`;
-            } else {
-                return `${rub} рубль ${kopeyki} копеек`;
+            rubName = 'рубль';
+            if (kopeyki === 0) {
+                return `${rub} ${rubName} ровно `;
             }
         } else if (rub % 100 >= 12 && rub % 100 <= 14) {
-            if (kopeyki % 10 == 1 && kopeyki % 100 !== 11 && kopeyki != 11) {
-                return `${rub} рублей ${kopeyki} копейка`;
-            } else if (kopeyki % 100 >= 12 && kopeyki % 100 <= 14) {
-                return `${rub} рублей ${kopeyki} копеек`;
-            } else if (kopeyki % 10 == 2 || kopeyki % 10 == 3 || kopeyki % 10 == 4) {
-                return `${rub} рублей ${kopeyki} копейки`;
-            } else {
-                return `${rub} рублей ${kopeyki} копеек`;
+            rubName = 'рублей';
+            if (kopeyki === 0) {
+                return `${rub} ${rubName} ровно `;
             }
         } else if (rub % 10 == 2 || rub % 10 == 3 || rub % 10 == 4) {
-            if (kopeyki % 10 == 1 && kopeyki % 100 !== 11 && kopeyki != 11) {
-                return `${rub} рубля ${kopeyki} копеек`;
-            } else if (kopeyki % 100 >= 12 && kopeyki % 100 <= 14) {
-                return `${rub} рубля ${kopeyki} копеек`;
-            } else if (kopeyki % 10 == 2 || kopeyki % 10 == 3 || kopeyki % 10 == 4) {
-                return `${rub} рубля ${kopeyki} копееки`;
-            } else {
-                return `${rub} рубля ${kopeyki} копеек`;
+            rubName = 'рубля';
+            if (kopeyki === 0) {
+                return `${rub} ${rubName} ровно `;
             }
-        } else if (kopeyki === 1) {
-            return `${rub} рублей ${kopeyki} копейка`;
-        } else if ((rub === 0 && kopeyki % 10 == 2) || kopeyki % 10 == 3 || kopeyki % 10 == 4) {
-            return `${rub} рублей ${kopeyki} копейки`;
         } else {
-            return `${rub} рублей ${kopeyki} копеек`;
+            rubName = 'рублей';
+            if (kopeyki === 0) {
+                return `${rub} ${rubName} ровно `;
+            }
+        }
+        if (kopeyki % 10 == 1 && kopeyki % 100 !== 11 && kopeyki != 11) {
+            kopName = 'копейка';
+        } else if (kopeyki % 100 >= 12 && kopeyki % 100 <= 14) {
+            kopName = 'копеек';
+        } else if (kopeyki % 10 == 2 || kopeyki % 10 == 3 || kopeyki % 10 == 4) {
+            kopName = 'копейки';
+        } else {
+            kopName = 'копеек';
         }
     }
+    return `${rub} ${rubName} ${kopeyki} ${kopName}`;
 }
