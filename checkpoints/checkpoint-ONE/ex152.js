d92c53fb4c312e76db2f8a8d3b8cf64ffa7e5c73
Mihail Prokopchik <raedar5555@gmail.com>
2023-04-24 11:22:57 +0300

    fix:ex152(Сделал ролл отдельной функцией)

--- checkpoints/checkpoint-ONE/ex152.js
+++ checkpoints/checkpoint-ONE/ex152.js
@@ -9,33 +9,35 @@ let bandit = {
     cylinder1: ['яблоко', 'банан', 'виноград', 'киви', 'вишня', 'лимон', 'груша', 'гранат', 'апельсин'],
     cylinder2: ['яблоко', 'банан', 'виноград', 'киви', 'вишня', 'лимон', 'груша', 'гранат', 'апельсин'],
     cylinder3: ['яблоко', 'банан', 'виноград', 'киви', 'вишня', 'лимон', 'груша', 'гранат', 'апельсин'],
-    roll() {
-        let result = [];
-        let rnd;
-        let fruit;
-        let scrolling = 0;
-        for (let index = 1; index <= 1000000; index++) {
-            scrolling++;
-            rnd = randomInteger(1, 8);
-            fruit = this.cylinder1[rnd];
-            result.push(fruit);
-            rnd = randomInteger(1, 8);
-            fruit = this.cylinder1[rnd];
-            result.push(fruit);
-            rnd = randomInteger(1, 8);
-            fruit = this.cylinder1[rnd];
-            result.push(fruit);
+};
+function roll(bandit) {
+    let result = [];
+    let rnd;
+    let fruit;
+    let scrolling = 0;
+    for (let index = 1; index <= 1000000; index++) {
+        scrolling++;
+        rnd = randomInteger(1, 8);
+        fruit = bandit.cylinder1[rnd];
+        result.push(fruit);
+        rnd = randomInteger(1, 8);
+        fruit = bandit.cylinder1[rnd];
+        result.push(fruit);
+        rnd = randomInteger(1, 8);
+        fruit = bandit.cylinder1[rnd];
+        result.push(fruit);
 
-            if (result.length === 3) {
-                if (result[0] === result[1] && result[1] === result[2]) {
-                    return `${result} ${scrolling}`;
-                } else {
-                    result = [];
-                }
+        if (result.length === 3) {
+            if (result[0] === result[1] && result[1] === result[2]) {
+                return `${result} ${scrolling}`;
+            } else {
+                result = [];
             }
         }
-    },
-};
+    }
+    return null;
+}
+
 function randomInteger(min, max) {
     let rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
