f6a712b4d5a86db0198f21e1e19037f716c75310
Mihail Prokopchik <raedar5555@gmail.com>
2023-05-22 19:33:10 +0300

    прототипы

---
+++ js/06-prototypes_inheritance/lectures/2023-05-22/script.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// let figure = {};
+
+// let triangle = {
+//     __proto__: figure,
+// };
+
+// let rightTriangle = {
+//     __proto__: triangle,
+// };
+
+// let quadrangle = {
+//     __proto__: figure,
+// };
+// let rectangle = {
+//     __proto__: quadrangle,
+// };
+// let square = {
+//     __proto__: rectangle,
+// };
+
+let transport = {};
+
+let ship = {
+    __proto__: transport,
+};
+
+let wheel = {
+    __proto__: transport,
+};
+
+let bicycle = {
+    __proto__: wheel,
+};
+
+let cars = {
+    __proto__: wheel,
+};
+
+let car1 = {
+    __proto__: cars,
+};
+
+let car2 = {
+    __proto__: cars,
+};
+
+let car2 = {};
