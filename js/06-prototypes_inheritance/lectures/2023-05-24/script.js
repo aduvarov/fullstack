b8feefb8455ff2847578a7a7b9e0ab0007044afa
Mihail Prokopchik <raedar5555@gmail.com>
2023-05-25 19:16:45 +0300

    Сделал модальное окно

--- js/06-prototypes_inheritance/lectures/2023-05-24/script.js
+++ js/06-prototypes_inheritance/lectures/2023-05-24/script.js
@@ -2,9 +2,16 @@
 // 501 movies total
 
 let container = document.querySelector('.container');
+let poster = document.querySelector('.poster');
+let block = document.querySelector('.block');
+let description = document.querySelector('.description');
+let h1 = document.querySelector('.name');
+let info = document.querySelector('.block__info');
+let actorsName = document.querySelector('.actor__name');
+let actorsPhoto = document.querySelector('.actor__photo');
 let input = document.querySelector('#num');
 let modalwindow = document.querySelector('.modalwindow');
-let maxFilms = 50; // сколько фильмов отрисовать на главной
+let maxFilms = 10; // сколько фильмов отрисовать на главной
 let filmCards;
 let uniqFilms;
 let allFilms;
@@ -31,7 +38,43 @@ modalwindow.addEventListener('click', () => {
     modalwindow.classList.add('invisible');
 });
 
-function renderModal(id) {}
+function renderModal(id) {
+    actorsName.innerHTML = 'Актёры:';
+    actorsPhoto.innerHTML = '';
+    h1.innerHTML = '';
+    info.innerHTML = '';
+    description.innerHTML = '';
+    let minutes = 0;
+    let hours = 0;
+    for (let index = 0; index < maxFilms; index++) {
+        if (uniqFilms[index].id == id) {
+            hours = uniqFilms[index].movieLength / 60;
+            minutes = uniqFilms[index].movieLength % 60;
+            poster.innerHTML = `<img src="${uniqFilms[index].poster.previewUrl}" alt="posterFilm" class="modal__poster"></img>`;
+            console.log(uniqFilms[index]);
+            h1.innerHTML = `<h1>${uniqFilms[index].name}</h1>`;
+            info.innerHTML += `<div class="type">Кинокритики: ${uniqFilms[index].rating.kp.toFixed(
+                1
+            )}, Кинопоиск: ${uniqFilms[index].rating.kp.toFixed(1)}.</div>`;
+            info.innerHTML += `<div class="type">${uniqFilms[index].year}</div>`;
+            info.innerHTML += `<div class="type"> ${uniqFilms[index].countries[0].name}</div>`;
+            info.innerHTML += `<div class="type">${uniqFilms[index].slogan}</div>`;
+            info.innerHTML += `<div class="type"></div>`;
+            info.innerHTML += `<div class="type">Часов: ${hours.toFixed(0)}, минут: ${minutes}</div>`;
+            info.innerHTML += `<div class="type"></div>`;
+            for (let count = 0; count < uniqFilms[index].genres.length; count++) {
+                info.innerHTML += `<span class="type">${uniqFilms[index].genres[count].name};</span>  `;
+            }
+            description.innerHTML = uniqFilms[index].description;
+            for (let count = 0; count < 5; count++) {
+                actorsName.innerHTML += `<span class="type">${uniqFilms[index].persons[count].name};</span> `;
+            }
+            for (let count = 0; count < 5; count++) {
+                actorsPhoto.innerHTML += `<img class="photo" src="${uniqFilms[index].persons[count].photo}">`;
+            }
+        }
+    }
+}
 
 function randomFilms(max) {
     let filmNum = 0;
