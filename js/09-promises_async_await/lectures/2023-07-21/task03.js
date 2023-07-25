7c1744943832344d8246f1192777a169926cf917
Mihail Prokopchik <raedar5555@gmail.com>
2023-07-25 17:01:27 +0300

    Пицерия

---
+++ js/09-promises_async_await/lectures/2023-07-21/task03.js
@@ -0,0 +1,25 @@
+'use strict';
+
+console.log('start');
+
+const promise1 = new Promise((resolve, reject) => {
+    console.log(1);
+    resolve(2);
+    console.log(3);
+});
+
+promise1.then(res => {
+    console.log(res);
+});
+
+console.log('end');
+
+/*
+
+start
+1
+end
+2
+3
+
+*/
---
+++ js/09-promises_async_await/lectures/2023-07-21/task06.js
@@ -0,0 +1,22 @@
+'use strict';
+
+console.log('start');
+
+Promise.resolve(1).then(res => {
+    console.log(res);
+});
+
+Promise.resolve(2).then(res => {
+    console.log(res);
+});
+
+console.log('end');
+
+/*
+
+start
+end 
+1
+2
+
+*/
