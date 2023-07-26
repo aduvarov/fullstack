0705c493c0aa6d571b87dd5b8b051bba4c3ddd33
Mihail Prokopchik <raedar5555@gmail.com>
2023-07-26 15:52:44 +0300

    Счётчики

--- js/09-promises_async_await/homeWorks/2023-07-24/script.js
+++ js/09-promises_async_await/homeWorks/2023-07-24/script.js
@@ -1,304 +1,134 @@
 'use strict';
 
-let num1 = document.querySelector('.number1');
-let num2 = document.querySelector('.number2');
-let num3 = document.querySelector('.number3');
-let num4 = document.querySelector('.number4');
-let plus1 = document.querySelector('.plus1');
-let minus1 = document.querySelector('.minus1');
+let hexade_num1 = document.querySelector('.hexade_num1');
+let hexade_num2 = document.querySelector('.hexade_num2');
 
-plus1.addEventListener('click', () => {
-    if (num1.innerHTML < 9) {
-        num1.innerHTML = +num1.innerHTML + 1;
-    } else {
-        if (num2.innerHTML < 9) {
-            num2.innerHTML++;
-            num1.innerHTML = 0;
-        } else {
-            if (num3.innerHTML < 9) {
-                num3.innerHTML++;
-                num2.innerHTML = 0;
-                num1.innerHTML = 0;
-            } else {
-                if (num4.innerHTML < 9) {
-                    num4.innerHTML++;
-                    num3.innerHTML = 0;
-                    num2.innerHTML = 0;
-                    num1.innerHTML = 0;
-                } else {
-                }
-            }
-        }
-    }
-});
-minus1.addEventListener('click', () => {
-    if (num1.innerHTML > 0) {
-        num1.innerHTML = +num1.innerHTML - 1;
-    } else {
-        if (num2.innerHTML > 0) {
-            if (num1.innerHTML > 0) {
-                num1.innerHTML = +num1.innerHTML - 1;
-            } else {
-                num2.innerHTML--;
-            }
-            num1.innerHTML = 9;
-        } else {
-            if (num1.innerHTML > 0) {
-                num1.innerHTML = +num1.innerHTML - 1;
-            } else if (num2.innerHTML > 0) {
-                num2.innerHTML = +num2.innerHTML - 1;
-            } else {
-                if (num3.innerHTML > 0) {
-                    num3.innerHTML--;
-                    num2.innerHTML = 9;
-                    num1.innerHTML = 9;
-                } else {
-                    if (num1.innerHTML > 0) {
-                        num1.innerHTML = +num1.innerHTML - 1;
-                    } else if (num2.innerHTML > 0) {
-                        num2.innerHTML = +num2.innerHTML - 1;
-                    } else if (num3.innerHTML > 0) {
-                        num3.innerHTML = +num3.innerHTML - 1;
-                    } else if (num4.innerHTML > 0) {
-                        num4.innerHTML--;
-                        num3.innerHTML = 9;
-                        num2.innerHTML = 9;
-                        num1.innerHTML = 9;
-                    }
-                }
-            }
-        }
-    }
-});
-let numb1 = document.querySelector('.num1');
-let numb2 = document.querySelector('.num2');
-let numb3 = document.querySelector('.num3');
-let numb4 = document.querySelector('.num4');
-let numb5 = document.querySelector('.num5');
-let numb6 = document.querySelector('.num6');
-let numb7 = document.querySelector('.num7');
-let numb8 = document.querySelector('.num8');
-let plus2 = document.querySelector('.plus2');
-let minus2 = document.querySelector('.minus2');
+let decimalNum1 = document.querySelector('.decimal_num1');
+let decimalNum2 = document.querySelector('.decimal_num2');
+let decimalNum3 = document.querySelector('.decimal_num3');
 
-plus2.addEventListener('click', () => {
-    if (numb1.innerHTML < 1) {
-        numb1.innerHTML = +numb1.innerHTML + 1;
+let binary_num1 = document.querySelector('.binary_num1');
+let binary_num2 = document.querySelector('.binary_num2');
+let binary_num3 = document.querySelector('.binary_num3');
+let binary_num4 = document.querySelector('.binary_num4');
+let binary_num5 = document.querySelector('.binary_num5');
+let binary_num6 = document.querySelector('.binary_num6');
+let binary_num7 = document.querySelector('.binary_num7');
+let binary_num8 = document.querySelector('.binary_num8');
+
+let plus = document.querySelector('.plus');
+let minus = document.querySelector('.minus');
+
+plus.addEventListener('click', () => {
+    let arr = [];
+    let num1 = +decimalNum1.innerHTML;
+    let num2 = +decimalNum2.innerHTML;
+    let num3 = +decimalNum3.innerHTML;
+    if (num3 === 2 && num2 === 5) {
+        if (num1 < 5) {
+            num1 = +num1 + 1;
+        }
     } else {
-        if (numb2.innerHTML < 1) {
-            numb2.innerHTML++;
-            numb1.innerHTML = 0;
+        if (num1 < 9) {
+            num1 = +num1 + 1;
         } else {
-            if (numb3.innerHTML < 1) {
-                numb3.innerHTML++;
-                numb2.innerHTML = 0;
-                numb1.innerHTML = 0;
+            if (num2 < 5) {
+                num2++;
+                num1 = 0;
             } else {
-                if (numb4.innerHTML < 1) {
-                    numb4.innerHTML++;
-                    numb3.innerHTML = 0;
-                    numb2.innerHTML = 0;
-                    numb1.innerHTML = 0;
-                } else {
-                    if (numb5.innerHTML < 1) {
-                        numb5.innerHTML++;
-                        numb4.innerHTML = 0;
-                        numb3.innerHTML = 0;
-                        numb2.innerHTML = 0;
-                        numb1.innerHTML = 0;
-                    } else {
-                        if (numb6.innerHTML < 1) {
-                            numb6.innerHTML++;
-                            numb5.innerHTML = 0;
-                            numb4.innerHTML = 0;
-                            numb3.innerHTML = 0;
-                            numb2.innerHTML = 0;
-                            numb1.innerHTML = 0;
-                        } else {
-                            if (numb7.innerHTML < 1) {
-                                numb7.innerHTML++;
-                                numb6.innerHTML = 0;
-                                numb5.innerHTML = 0;
-                                numb4.innerHTML = 0;
-                                numb3.innerHTML = 0;
-                                numb2.innerHTML = 0;
-                                numb1.innerHTML = 0;
-                            } else {
-                                if (numb8.innerHTML < 1) {
-                                    numb8.innerHTML++;
-                                    numb7.innerHTML = 0;
-                                    numb6.innerHTML = 0;
-                                    numb5.innerHTML = 0;
-                                    numb4.innerHTML = 0;
-                                    numb3.innerHTML = 0;
-                                    numb2.innerHTML = 0;
-                                    numb1.innerHTML = 0;
-                                }
-                            }
-                        }
-                    }
+                if (num3 < 3) {
+                    num3++;
+                    num2 = 0;
+                    num1 = 0;
                 }
             }
         }
     }
+    arr.push(num1, num2, num3);
+    decimalNum1.innerHTML = arr[0];
+    decimalNum2.innerHTML = arr[1];
+    decimalNum3.innerHTML = arr[2];
+    let result = arr.reverse().join('');
+    result = +result;
+    arr = result.toString(2).split('').reverse();
+    if (arr.length < 8) {
+        for (let i = arr.length; arr.length < 8; i++) {
+            arr.push(0);
+        }
+    }
+    binary_num1.innerHTML = arr[0];
+    binary_num2.innerHTML = arr[1];
+    binary_num3.innerHTML = arr[2];
+    binary_num4.innerHTML = arr[3];
+    binary_num5.innerHTML = arr[4];
+    binary_num6.innerHTML = arr[5];
+    binary_num7.innerHTML = arr[6];
+    binary_num8.innerHTML = arr[7];
+    arr = result.toString(16).split('').reverse();
+    if (arr.length < 2) {
+        for (let i = arr.length; arr.length < 2; i++) {
+            arr.push(0);
+        }
+    }
+    hexade_num1.innerHTML = arr[0];
+    hexade_num2.innerHTML = arr[1];
 });
 
-minus2.addEventListener('click', () => {
-    if (numb1.innerHTML > 0) {
-        numb1.innerHTML = +numb1.innerHTML - 1;
+minus.addEventListener('click', () => {
+    let arr = [];
+    let num1 = +decimalNum1.innerHTML;
+    let num2 = +decimalNum2.innerHTML;
+    let num3 = +decimalNum3.innerHTML;
+    if (num1 > 0) {
+        num1 = +num1 - 1;
     } else {
-        if (numb2.innerHTML > 0) {
-            if (numb1.innerHTML > 0) {
-                numb1.innerHTML = +numb1.innerHTML - 1;
+        if (num2 > 0) {
+            if (num1 > 0) {
+                num1 = +num1 - 1;
             } else {
-                numb2.innerHTML--;
-                numb1.innerHTML = 1;
+                num2--;
             }
+            num1 = 9;
         } else {
-            if (numb1.innerHTML > 0) {
-                numb1.innerHTML = +numb1.innerHTML - 1;
-            } else if (num2.innerHTML > 0) {
-                numb2.innerHTML = +numb2.innerHTML - 1;
+            if (num1 > 0) {
+                num1 = +num1 - 1;
+            } else if (num2 > 0) {
+                num2 = +num2 - 1;
             } else {
-                if (numb3.innerHTML > 0) {
-                    numb3.innerHTML--;
-                    numb2.innerHTML = 1;
-                    numb1.innerHTML = 1;
-                } else {
-                    if (numb1.innerHTML > 0) {
-                        numb1.innerHTML = +numb1.innerHTML - 1;
-                    } else if (numb2.innerHTML > 0) {
-                        numb2.innerHTML = +numb2.innerHTML - 1;
-                    } else if (numb3.innerHTML > 0) {
-                        numb3.innerHTML = +numb3.innerHTML - 1;
-                    } else if (numb4.innerHTML > 0) {
-                        numb4.innerHTML--;
-                        numb3.innerHTML = 1;
-                        numb2.innerHTML = 1;
-                        numb1.innerHTML = 1;
-                    } else {
-                        if (numb5.innerHTML > 0) {
-                            numb5.innerHTML--;
-                            numb4.innerHTML = 1;
-                            numb3.innerHTML = 1;
-                            numb2.innerHTML = 1;
-                            numb1.innerHTML = 1;
-                        } else {
-                            if (numb1.innerHTML > 0) {
-                                numb1.innerHTML = +numb1.innerHTML - 1;
-                            } else if (numb2.innerHTML > 0) {
-                                numb2.innerHTML = +numb2.innerHTML - 1;
-                            } else if (numb3.innerHTML > 0) {
-                                numb3.innerHTML = +numb3.innerHTML - 1;
-                            } else if (numb4.innerHTML > 0) {
-                                numb4.innerHTML = +numb4.innerHTML - 1;
-                            } else if (numb5.innerHTML > 0) {
-                                numb5.innerHTML--;
-                                numb4.innerHTML = 1;
-                                numb3.innerHTML = 1;
-                                numb2.innerHTML = 1;
-                                numb1.innerHTML = 1;
-                            } else {
-                                if (numb6.innerHTML > 0) {
-                                    numb6.innerHTML--;
-                                    numb5.innerHTML = 1;
-                                    numb4.innerHTML = 1;
-                                    numb3.innerHTML = 1;
-                                    numb2.innerHTML = 1;
-                                    numb1.innerHTML = 1;
-                                } else {
-                                    if (numb1.innerHTML > 0) {
-                                        numb1.innerHTML = +numb1.innerHTML - 1;
-                                    } else if (numb2.innerHTML > 0) {
-                                        numb2.innerHTML = +numb2.innerHTML - 1;
-                                    } else if (numb3.innerHTML > 0) {
-                                        numb3.innerHTML = +numb3.innerHTML - 1;
-                                    } else if (numb4.innerHTML > 0) {
-                                        numb4.innerHTML = +numb4.innerHTML - 1;
-                                    } else if (numb5.innerHTML > 0) {
-                                        numb5.innerHTML = +numb5.innerHTML - 1;
-                                    } else if (numb6.innerHTML > 0) {
-                                        numb6.innerHTML--;
-                                        numb5.innerHTML = 1;
-                                        numb4.innerHTML = 1;
-                                        numb3.innerHTML = 1;
-                                        numb2.innerHTML = 1;
-                                        numb1.innerHTML = 1;
-                                    } else {
-                                        if (numb7.innerHTML > 0) {
-                                            numb7.innerHTML--;
-                                            numb6.innerHTML = 1;
-                                            numb5.innerHTML = 1;
-                                            numb4.innerHTML = 1;
-                                            numb3.innerHTML = 1;
-                                            numb2.innerHTML = 1;
-                                            numb1.innerHTML = 1;
-                                        } else {
-                                            if (numb1.innerHTML > 0) {
-                                                numb1.innerHTML = +numb1.innerHTML - 1;
-                                            } else if (numb2.innerHTML > 0) {
-                                                numb2.innerHTML = +numb2.innerHTML - 1;
-                                            } else if (numb3.innerHTML > 0) {
-                                                numb3.innerHTML = +numb3.innerHTML - 1;
-                                            } else if (numb4.innerHTML > 0) {
-                                                numb4.innerHTML = +numb4.innerHTML - 1;
-                                            } else if (numb5.innerHTML > 0) {
-                                                numb5.innerHTML = +numb5.innerHTML - 1;
-                                            } else if (numb6.innerHTML > 0) {
-                                                numb6.innerHTML = +numb6.innerHTML - 1;
-                                            } else if (numb7.innerHTML > 0) {
-                                                numb7.innerHTML--;
-                                                numb6.innerHTML = 1;
-                                                numb5.innerHTML = 1;
-                                                numb4.innerHTML = 1;
-                                                numb3.innerHTML = 1;
-                                                numb2.innerHTML = 1;
-                                                numb1.innerHTML = 1;
-                                            } else {
-                                                if (numb8.innerHTML > 0) {
-                                                    numb8.innerHTML--;
-                                                    numb7.innerHTML = 1;
-                                                    numb6.innerHTML = 1;
-                                                    numb5.innerHTML = 1;
-                                                    numb4.innerHTML = 1;
-                                                    numb3.innerHTML = 1;
-                                                    numb2.innerHTML = 1;
-                                                    numb1.innerHTML = 1;
-                                                } else {
-                                                    if (numb1.innerHTML > 0) {
-                                                        numb1.innerHTML = +numb1.innerHTML - 1;
-                                                    } else if (numb2.innerHTML > 0) {
-                                                        numb2.innerHTML = +numb2.innerHTML - 1;
-                                                    } else if (numb3.innerHTML > 0) {
-                                                        numb3.innerHTML = +numb3.innerHTML - 1;
-                                                    } else if (numb4.innerHTML > 0) {
-                                                        numb4.innerHTML = +numb4.innerHTML - 1;
-                                                    } else if (numb5.innerHTML > 0) {
-                                                        numb5.innerHTML = +numb5.innerHTML - 1;
-                                                    } else if (numb6.innerHTML > 0) {
-                                                        numb6.innerHTML = +numb6.innerHTML - 1;
-                                                    } else if (numb7.innerHTML > 0) {
-                                                        numb7.innerHTML = +numb7.innerHTML - 1;
-                                                    } else if (numb8.innerHTML > 0) {
-                                                        numb8.innerHTML--;
-                                                        numb7.innerHTML = 1;
-                                                        numb6.innerHTML = 1;
-                                                        numb5.innerHTML = 1;
-                                                        numb4.innerHTML = 1;
-                                                        numb3.innerHTML = 1;
-                                                        numb2.innerHTML = 1;
-                                                        numb1.innerHTML = 1;
-                                                    }
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+                if (num3 > 0) {
+                    num3--;
+                    num2 = 9;
+                    num1 = 9;
                 }
             }
         }
     }
+    arr.push(num1, num2, num3);
+    decimalNum1.innerHTML = arr[0];
+    decimalNum2.innerHTML = arr[1];
+    decimalNum3.innerHTML = arr[2];
+    let result = arr.reverse().join('');
+    result = +result;
+    arr = result.toString(2).split('').reverse();
+    if (arr.length < 8) {
+        for (let i = arr.length; arr.length < 8; i++) {
+            arr.push(0);
+        }
+    }
+    binary_num1.innerHTML = arr[0];
+    binary_num2.innerHTML = arr[1];
+    binary_num3.innerHTML = arr[2];
+    binary_num4.innerHTML = arr[3];
+    binary_num5.innerHTML = arr[4];
+    binary_num6.innerHTML = arr[5];
+    binary_num7.innerHTML = arr[6];
+    binary_num8.innerHTML = arr[7];
+    arr = result.toString(16).split('').reverse();
+    if (arr.length < 2) {
+        for (let i = arr.length; arr.length < 2; i++) {
+            arr.push(0);
+        }
+    }
+    hexade_num1.innerHTML = arr[0];
+    hexade_num2.innerHTML = arr[1];
 });
