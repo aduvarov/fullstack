103f2a942c8ad7141c2bc3446624a65bb2d9ed93
Mihail Prokopchik <raedar5555@gmail.com>
2023-08-01 16:28:38 +0300

    Счётчики

--- js/09-promises_async_await/homeWorks/2023-07-24/script.js
+++ js/09-promises_async_await/homeWorks/2023-07-24/script.js
@@ -1,15 +1,15 @@
-'use strict';
+import { realBoard } from "./module/realboard.js";
 
 let hexade_num1 = document.querySelector('.hexade_num1');
 let hexade_num2 = document.querySelector('.hexade_num2');
 
 let decimalNum1 = document.querySelector('.decimal_num1');
 let decimalNum2 = document.querySelector('.decimal_num2');
-let decimalNum3 = document.querySelector('.decimal_num3');
+let decimalNum3= document.querySelector('.decimal_num3');
 
 let binary_num1 = document.querySelector('.binary_num1');
 let binary_num2 = document.querySelector('.binary_num2');
-let binary_num3 = document.querySelector('.binary_num3');
+let binary_num3= document.querySelector('.binary_num3');
 let binary_num4 = document.querySelector('.binary_num4');
 let binary_num5 = document.querySelector('.binary_num5');
 let binary_num6 = document.querySelector('.binary_num6');
@@ -18,36 +18,54 @@ let binary_num8 = document.querySelector('.binary_num8');
 
 let plus = document.querySelector('.plus');
 let minus = document.querySelector('.minus');
+let viewBoard = [];
+for (let i = 1; i < 4; i++) {
+    viewBoard.push(document.querySelector(`.decimal_num${i}`));
+}
+for (let i = 1; i < 3; i++) {
+    viewBoard.push(document.querySelector(`.hexade_num${i}`));
+}
+for (let i = 1; i < 9; i++) {
+    viewBoard.push(document.querySelector(`.binary_num${i}`));
+    
+}
+
+
+function renderNumber() {
+    realBoard.forEach((e, i) => {
+        viewBoard[i].innerText = e;
+    });
+}
 
 plus.addEventListener('click', () => {
     let arr = [];
-    let num1 = +decimalNum1.innerHTML;
-    let num2 = +decimalNum2.innerHTML;
-    let num3 = +decimalNum3.innerHTML;
-    if (num3 === 2 && num2 === 5) {
-        if (num1 < 5) {
-            num1 = +num1 + 1;
+     realBoard[0]= +decimalNum1.innerHTML;
+     realBoard[1]= +decimalNum2.innerHTML;
+     realBoard[2]= +decimalNum3.innerHTML;
+    if (realBoard[2]=== 2 && realBoard[1] === 5) {
+        if (realBoard[0]< 5) {
+            realBoard[0]= realBoard[0]+ 1;
         }
     } else {
-        if (num1 < 9) {
-            num1 = +num1 + 1;
+        if (realBoard[0]< 9) {
+            realBoard[0]= realBoard[0]+ 1;
         } else {
-            if (num2 < 5) {
-                num2++;
-                num1 = 0;
+            if (realBoard[1]< 5) {
+                realBoard[1]++;
+                realBoard[0]= 0;
             } else {
-                if (num3 < 3) {
-                    num3++;
-                    num2 = 0;
-                    num1 = 0;
+                if (realBoard[2]< 3) {
+                    realBoard[2]++;
+                    realBoard[1]= 0;
+                    realBoard[0]= 0;
                 }
             }
         }
     }
-    arr.push(num1, num2, num3);
-    decimalNum1.innerHTML = arr[0];
-    decimalNum2.innerHTML = arr[1];
-    decimalNum3.innerHTML = arr[2];
+    arr.push(realBoard[0], realBoard[1], realBoard[2]);
+    for(let i = 0;i < 3;i++){
+        realBoard[i] = +arr[i]
+    }
     let result = arr.reverse().join('');
     result = +result;
     arr = result.toString(2).split('').reverse();
@@ -56,57 +74,59 @@ plus.addEventListener('click', () => {
             arr.push(0);
         }
     }
-    binary_num1.innerHTML = arr[0];
-    binary_num2.innerHTML = arr[1];
-    binary_num3.innerHTML = arr[2];
-    binary_num4.innerHTML = arr[3];
-    binary_num5.innerHTML = arr[4];
-    binary_num6.innerHTML = arr[5];
-    binary_num7.innerHTML = arr[6];
-    binary_num8.innerHTML = arr[7];
+    let index = 0;
+    for(let i = 5;i < 12;i++){
+        realBoard[i] = +arr[index]
+        index++
+    }
+
     arr = result.toString(16).split('').reverse();
     if (arr.length < 2) {
         for (let i = arr.length; arr.length < 2; i++) {
             arr.push(0);
         }
     }
-    hexade_num1.innerHTML = arr[0];
-    hexade_num2.innerHTML = arr[1];
+    index = 0;
+    for(let i = 3;i < 5;i++){
+        realBoard[i] = arr[index]
+        index++
+    }
+   renderNumber()
 });
 
 minus.addEventListener('click', () => {
     let arr = [];
-    let num1 = +decimalNum1.innerHTML;
-    let num2 = +decimalNum2.innerHTML;
-    let num3 = +decimalNum3.innerHTML;
-    if (num1 > 0) {
-        num1 = +num1 - 1;
+     realBoard[0]= +decimalNum1.innerHTML;
+     realBoard[1]= +decimalNum2.innerHTML;
+    realBoard[2]= +decimalNum3.innerHTML;
+    if (realBoard[0]> 0) {
+        realBoard[0]= +realBoard[0]- 1;
     } else {
-        if (num2 > 0) {
-            if (num1 > 0) {
-                num1 = +num1 - 1;
+        if (realBoard[1]> 0) {
+            if (realBoard[0]> 0) {
+                realBoard[0]= +realBoard[0]- 1;
             } else {
-                num2--;
+                realBoard[1]--;
             }
-            num1 = 9;
+            realBoard[0]= 9;
         } else {
-            if (num1 > 0) {
-                num1 = +num1 - 1;
-            } else if (num2 > 0) {
-                num2 = +num2 - 1;
+            if (realBoard[0]> 0) {
+                realBoard[0]= +realBoard[0]- 1;
+            } else if (realBoard[1]> 0) {
+                realBoard[1]= +realBoard[1]- 1;
             } else {
-                if (num3 > 0) {
-                    num3--;
-                    num2 = 9;
-                    num1 = 9;
+                if (realBoard[2]> 0) {
+                    realBoard[2]--;
+                    realBoard[1]= 9;
+                    realBoard[0]= 9;
                 }
             }
         }
     }
-    arr.push(num1, num2, num3);
-    decimalNum1.innerHTML = arr[0];
-    decimalNum2.innerHTML = arr[1];
-    decimalNum3.innerHTML = arr[2];
+    arr.push(realBoard[0], realBoard[1], realBoard[2]);
+    for(let i = 0;i < 3;i++){
+        realBoard[i] = +arr[i]
+    }
     let result = arr.reverse().join('');
     result = +result;
     arr = result.toString(2).split('').reverse();
@@ -115,20 +135,22 @@ minus.addEventListener('click', () => {
             arr.push(0);
         }
     }
-    binary_num1.innerHTML = arr[0];
-    binary_num2.innerHTML = arr[1];
-    binary_num3.innerHTML = arr[2];
-    binary_num4.innerHTML = arr[3];
-    binary_num5.innerHTML = arr[4];
-    binary_num6.innerHTML = arr[5];
-    binary_num7.innerHTML = arr[6];
-    binary_num8.innerHTML = arr[7];
+    let index = 0;
+    for(let i = 5;i < 12;i++){
+        realBoard[i] = +arr[index]
+        index++
+    }
+
     arr = result.toString(16).split('').reverse();
     if (arr.length < 2) {
         for (let i = arr.length; arr.length < 2; i++) {
             arr.push(0);
         }
     }
-    hexade_num1.innerHTML = arr[0];
-    hexade_num2.innerHTML = arr[1];
+    index = 0;
+    for(let i = 3;i < 5;i++){
+        realBoard[i] = arr[index]
+        index++
+    }
+   renderNumber()
 });
